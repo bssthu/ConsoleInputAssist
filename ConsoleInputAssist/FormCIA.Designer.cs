bf58e3b64aa42443dff05b532afc88cdf2019c5b
bssthu <bssda03@gmail.com>
2015-01-02 21:03:17 +0800

    获取hwnd

--- ConsoleInputAssist/FormCIA.Designer.cs
+++ ConsoleInputAssist/FormCIA.Designer.cs
@@ -28,20 +28,61 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.buttonGetWindowName = new System.Windows.Forms.Button();
+            this.textBoxWindowName = new System.Windows.Forms.TextBox();
+            this.textBoxHwnd = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
-            // Form1
+            // buttonGetWindowName
+            // 
+            this.buttonGetWindowName.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonGetWindowName.Location = new System.Drawing.Point(358, 11);
+            this.buttonGetWindowName.Name = "buttonGetWindowName";
+            this.buttonGetWindowName.Size = new System.Drawing.Size(102, 23);
+            this.buttonGetWindowName.TabIndex = 0;
+            this.buttonGetWindowName.Text = "捕获窗口(&G)";
+            this.buttonGetWindowName.UseVisualStyleBackColor = true;
+            this.buttonGetWindowName.Click += new System.EventHandler(this.buttonGetWindowName_Click);
+            // 
+            // textBoxWindowName
+            // 
+            this.textBoxWindowName.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxWindowName.Location = new System.Drawing.Point(13, 13);
+            this.textBoxWindowName.Name = "textBoxWindowName";
+            this.textBoxWindowName.ReadOnly = true;
+            this.textBoxWindowName.Size = new System.Drawing.Size(237, 21);
+            this.textBoxWindowName.TabIndex = 1;
+            // 
+            // textBoxHwnd
+            // 
+            this.textBoxHwnd.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxHwnd.Location = new System.Drawing.Point(256, 13);
+            this.textBoxHwnd.Name = "textBoxHwnd";
+            this.textBoxHwnd.ReadOnly = true;
+            this.textBoxHwnd.Size = new System.Drawing.Size(96, 21);
+            this.textBoxHwnd.TabIndex = 2;
+            // 
+            // FormCIA
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
-            this.Name = "Form1";
+            this.ClientSize = new System.Drawing.Size(472, 292);
+            this.Controls.Add(this.textBoxHwnd);
+            this.Controls.Add(this.textBoxWindowName);
+            this.Controls.Add(this.buttonGetWindowName);
+            this.Name = "FormCIA";
             this.Text = "ConsoleInputAssist by bss";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button buttonGetWindowName;
+        private System.Windows.Forms.TextBox textBoxWindowName;
+        private System.Windows.Forms.TextBox textBoxHwnd;
     }
 }
 
--- ConsoleInputAssist/FormCIA.cs
+++ ConsoleInputAssist/FormCIA.cs
@@ -2,8 +2,10 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,9 +14,57 @@ namespace ConsoleInputAssist
 {
     public partial class FormCIA : Form
     {
+        [DllImport("user32.dll", EntryPoint = "GetForegroundWindow")]
+        static extern IntPtr GetForegroundWindow();
+
+        [DllImport("user32.dll", EntryPoint = "GetWindowTextLength")]
+        static extern int GetWindowTextLength(IntPtr hwnd);
+
+        [DllImport("user32.dll", EntryPoint = "GetWindowText")]
+        static extern int GetWindowText(IntPtr hwnd, StringBuilder lpString, int nMaxCount);
+
+        private IntPtr hwndAct = IntPtr.Zero;
+        private bool isCatching = false;
+
         public FormCIA()
         {
             InitializeComponent();
+        }
+
+        private void formDeactivate(object sender, EventArgs e)
+        {
+            this.Deactivate -= new EventHandler(formDeactivate);
+
+            hwndAct = GetForegroundWindow();
+            textBoxHwnd.Text = "0x" + hwndAct.ToString("x");
+
+            int textLength = GetWindowTextLength(hwndAct);
+            StringBuilder str = new StringBuilder(textLength + 1);
+            try
+            {
+                GetWindowText(hwndAct, str, str.Capacity);
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+            }
+
+            textBoxWindowName.Text = str.ToString();
+            isCatching = false;
+            buttonGetWindowName.Enabled = true;
+        }
+
+        private void buttonGetWindowName_Click(object sender, EventArgs e)
+        {
+            if (!isCatching)
+            {
+                isCatching = true;
+                buttonGetWindowName.Enabled = false;
+                hwndAct = IntPtr.Zero;
+                textBoxWindowName.Text = "";
+                textBoxHwnd.Text = "";
+                this.Deactivate += new EventHandler(formDeactivate);
+            }
         }
     }
 }

a403375f235b3ce4bccec8e84f87dbb44bba583b
bssthu <bssthu@users.noreply.github.com>
2015-04-11 12:19:37 +0800

    move API to class WinAPI

--- ConsoleInputAssist/FormCIA_func.cs
+++ ConsoleInputAssist/FormCIA_func.cs
@@ -15,80 +15,6 @@ namespace ConsoleInputAssist
 {
 	public partial class FormCIA
     {
-        [DllImport("user32.dll", EntryPoint = "GetForegroundWindow")]
-        static extern IntPtr GetForegroundWindow();
-
-        [DllImport("user32.dll", EntryPoint = "SetForegroundWindow")]
-        static extern int SetForegroundWindow(IntPtr hwnd);
-
-        [DllImport("user32.dll", EntryPoint = "GetWindowTextLength")]
-        static extern int GetWindowTextLength(IntPtr hwnd);
-
-        [DllImport("user32.dll", EntryPoint = "GetWindowText")]
-        static extern int GetWindowText(IntPtr hwnd, StringBuilder lpString, int nMaxCount);
-
-        [DllImport("User32.DLL")]
-        static extern int SendMessage(IntPtr hWnd, uint Msg, int wParam, string lParam);
-
-        [DllImport("User32.DLL")]
-        static extern int SendMessage(IntPtr hWnd, uint Msg, int wParam, int lParam);
-
-        [DllImport("User32.DLL", EntryPoint = "SendInput")]
-        static extern uint SendInput(uint nInputs, INPUT[] pInputs, int cbSize);
-
-        [DllImport("User32.DLL", EntryPoint = "GetKeyState")]
-        static extern short GetKeyState(int nVirtKey);
-
-        [StructLayout(LayoutKind.Sequential)]
-        struct INPUT
-        {
-            public uint Type;   // 0=INPUT_MOUSE, 1=INPUT_KEYBOARD, 2=INPUT_HARDWARE
-            public UNIONINPUT ui;
-        }
-
-        [StructLayout(LayoutKind.Explicit)]
-        struct UNIONINPUT
-        {
-            [FieldOffset(0)]
-            public MOUSEINPUT mi;
-            [FieldOffset(0)]
-            public KEYBDINPUT ki;
-            [FieldOffset(0)]
-            public HARDWAREINPUT hi;
-        }
-        [StructLayout(LayoutKind.Sequential)]
-        struct MOUSEINPUT
-        {
-            public int dx;
-            public int dy;
-            public uint mouseData;
-            public uint dwFlags;
-            public uint time;
-            public IntPtr dwExtraInfo;
-        }
-        [StructLayout(LayoutKind.Sequential)]
-        struct KEYBDINPUT
-        {
-            public ushort wVk;
-            public ushort wScan;
-            public uint dwFlags;
-            public uint time;
-            public IntPtr dwExtraInfo;
-        }
-        [StructLayout(LayoutKind.Sequential)]
-        struct HARDWAREINPUT
-        {
-            public uint uMsg;
-            public ushort wParamL;
-            public ushort wParamH;
-        }
-
-        public const uint WM_SETTEXT = 0x000C;
-        public const uint WM_CHAR = 0x0102;
-        public const uint KEYEVENTF_KEYUP = 2;
-        public const int VK_CAPITAL = 0x14;
-        public const int VK_LSHIFT = 0xA0;
-
         private IntPtr hwndAct = IntPtr.Zero;
         private bool isCatching = false;
 
@@ -104,15 +30,15 @@ namespace ConsoleInputAssist
             this.Deactivate -= new EventHandler(formDeactivate);
 
             // get hwnd
-            hwndAct = GetForegroundWindow();
+            hwndAct = WinAPI.GetForegroundWindow();
             textBoxHwnd.Text = "0x" + hwndAct.ToString("x");
 
             // get window text
-            int textLength = GetWindowTextLength(hwndAct);
+            int textLength = WinAPI.GetWindowTextLength(hwndAct);
             StringBuilder str = new StringBuilder(textLength + 1);
             try
             {
-                GetWindowText(hwndAct, str, str.Capacity);
+                WinAPI.GetWindowText(hwndAct, str, str.Capacity);
             }
             catch (Exception ex)
             {
@@ -172,19 +98,19 @@ namespace ConsoleInputAssist
         {
             for (int i = 0; i < text.Length; i++)
             {
-                SendMessage(hwndAct, WM_CHAR, (int)text[i], 0);
+                WinAPI.SendMessage(hwndAct, WinAPI.WM_CHAR, (int)text[i], 0);
             }
         }
 
         private void sendTextInput(IntPtr hwnd, String text)
         {
-            INPUT inputShift = new INPUT();
+            WinAPI.INPUT inputShift = new WinAPI.INPUT();
             inputShift.Type = 1;
-            inputShift.ui.ki.wVk = VK_LSHIFT;
-            INPUT[] theShiftInput = new INPUT[] { inputShift };
+            inputShift.ui.ki.wVk = WinAPI.VK_LSHIFT;
+            WinAPI.INPUT[] theShiftInput = new WinAPI.INPUT[] { inputShift };
 
-            INPUT input = new INPUT();
-            SetForegroundWindow(hwnd);
+            WinAPI.INPUT input = new WinAPI.INPUT();
+            WinAPI.SetForegroundWindow(hwnd);
             for (int i = 0; i < text.Length; i++)
             {
                 input.Type = 1;
@@ -193,25 +119,26 @@ namespace ConsoleInputAssist
                 {
                     input.ui.ki.wVk = char.ToUpper(text[i]);
                 }
-                bool pressShift = char.IsLetter(text[i]) && ((GetKeyState(VK_CAPITAL) != 0) ^ char.IsUpper(text[i])); // upper case
+                bool pressShift = char.IsLetter(text[i]) &&
+                        ((WinAPI.GetKeyState(WinAPI.VK_CAPITAL) != 0) ^ char.IsUpper(text[i])); // upper case
                 
                 // shift down (if neccessary)
                 if (pressShift)
                 {
                     theShiftInput[0].ui.ki.dwFlags = 0;
-                    SendInput(1, theShiftInput, Marshal.SizeOf(inputShift));
+                    WinAPI.SendInput(1, theShiftInput, Marshal.SizeOf(inputShift));
                 }
                 // key down
-                INPUT[] theInput = new INPUT[] { input };
-                SendInput(1, theInput, Marshal.SizeOf(input));
+                WinAPI.INPUT[] theInput = new WinAPI.INPUT[] { input };
+                WinAPI.SendInput(1, theInput, Marshal.SizeOf(input));
                 // key up
-                theInput[0].ui.ki.dwFlags = KEYEVENTF_KEYUP;
-                SendInput(1, theInput, Marshal.SizeOf(input));
+                theInput[0].ui.ki.dwFlags = WinAPI.KEYEVENTF_KEYUP;
+                WinAPI.SendInput(1, theInput, Marshal.SizeOf(input));
                 // shift up
                 if (pressShift)
                 {
-                    theShiftInput[0].ui.ki.dwFlags = KEYEVENTF_KEYUP;
-                    SendInput(1, theShiftInput, Marshal.SizeOf(inputShift));
+                    theShiftInput[0].ui.ki.dwFlags = WinAPI.KEYEVENTF_KEYUP;
+                    WinAPI.SendInput(1, theShiftInput, Marshal.SizeOf(inputShift));
                 }
             }
         }
---
+++ ConsoleInputAssist/WinAPI.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace ConsoleInputAssist
+{
+    public static class WinAPI
+    {
+        [DllImport("user32.dll", EntryPoint = "GetForegroundWindow")]
+        public static extern IntPtr GetForegroundWindow();
+
+        [DllImport("user32.dll", EntryPoint = "SetForegroundWindow")]
+        public static extern int SetForegroundWindow(IntPtr hwnd);
+
+        [DllImport("user32.dll", EntryPoint = "GetWindowTextLength")]
+        public static extern int GetWindowTextLength(IntPtr hwnd);
+
+        [DllImport("user32.dll", EntryPoint = "GetWindowText")]
+        public static extern int GetWindowText(IntPtr hwnd, StringBuilder lpString, int nMaxCount);
+
+        [DllImport("User32.DLL")]
+        public static extern int SendMessage(IntPtr hWnd, uint Msg, int wParam, string lParam);
+
+        [DllImport("User32.DLL")]
+        public static extern int SendMessage(IntPtr hWnd, uint Msg, int wParam, int lParam);
+
+        [DllImport("User32.DLL", EntryPoint = "SendInput")]
+        public static extern uint SendInput(uint nInputs, INPUT[] pInputs, int cbSize);
+
+        [DllImport("User32.DLL", EntryPoint = "GetKeyState")]
+        public static extern short GetKeyState(int nVirtKey);
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct INPUT
+        {
+            public uint Type;   // 0=INPUT_MOUSE, 1=INPUT_KEYBOARD, 2=INPUT_HARDWARE
+            public UNIONINPUT ui;
+        }
+
+        [StructLayout(LayoutKind.Explicit)]
+        public struct UNIONINPUT
+        {
+            [FieldOffset(0)]
+            public MOUSEINPUT mi;
+            [FieldOffset(0)]
+            public KEYBDINPUT ki;
+            [FieldOffset(0)]
+            public HARDWAREINPUT hi;
+        }
+        [StructLayout(LayoutKind.Sequential)]
+        public struct MOUSEINPUT
+        {
+            public int dx;
+            public int dy;
+            public uint mouseData;
+            public uint dwFlags;
+            public uint time;
+            public IntPtr dwExtraInfo;
+        }
+        [StructLayout(LayoutKind.Sequential)]
+        public struct KEYBDINPUT
+        {
+            public ushort wVk;
+            public ushort wScan;
+            public uint dwFlags;
+            public uint time;
+            public IntPtr dwExtraInfo;
+        }
+        [StructLayout(LayoutKind.Sequential)]
+        public struct HARDWAREINPUT
+        {
+            public uint uMsg;
+            public ushort wParamL;
+            public ushort wParamH;
+        }
+
+        public const uint WM_SETTEXT = 0x000C;
+        public const uint WM_CHAR = 0x0102;
+        public const uint KEYEVENTF_KEYUP = 2;
+        public const int VK_CAPITAL = 0x14;
+        public const int VK_LSHIFT = 0xA0;
+    }
+}
